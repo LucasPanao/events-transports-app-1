39ce94da172522995f286e18d835c667f7ea0fc2
Lucas Panao <panaotrabalho@gmail.com>
2024-06-06 19:21:20 -0300

    updating body about faq and feedbacks

--- src/app/body/body.component.ts
+++ src/app/body/body.component.ts
@@ -15,11 +15,53 @@ export class BodyComponent {
       info: 'Additional information about Post 1',
     },
     {
-      photo: 'https://via.placeholder.com/150',
-      name: 'Post 2',
+      photo: './assets/luan.jpg',
+      name: 'Post 1',
       date: new Date(),
-      value: 200,
-      info: 'Additional information about Post 2',
+      value: 100,
+      info: 'Additional information about Post 1',
     },
+    {
+      photo: './assets/anitta.png',
+      name: 'Post 1',
+      date: new Date(),
+      value: 100,
+      info: 'Additional information about Post 1',
+    },
+    {
+      photo: './assets/luisa.jpg',
+      name: 'Post 1',
+      date: new Date(),
+      value: 100,
+      info: 'Additional information about Post 1',
+    },
+    {
+      photo: './assets/alok.png',
+      name: 'Post 1',
+      date: new Date(),
+      value: 100,
+      info: 'Additional information about Post 1',
+    },
+    {
+      photo: './assets/luan.jpg',
+      name: 'Post 1',
+      date: new Date(),
+      value: 100,
+      info: 'Additional information about Post 1',
+    },
+    {
+      photo: './assets/anitta.png',
+      name: 'Post 1',
+      date: new Date(),
+      value: 100,
+      info: 'Additional information about Post 1',
+    },
+    {
+      photo: './assets/luisa.jpg',
+      name: 'Post 1',
+      date: new Date(),
+      value: 100,
+      info: 'Additional information about Post 1',
+    }
   ];
 }
--- src/app/feedbacks/feedbacks.component.ts
+++ src/app/feedbacks/feedbacks.component.ts
@@ -1,10 +1,48 @@
 import { Component } from '@angular/core';
 
+interface Feedback {
+  name: string;
+  photo: string;
+  text: string;
+}
+
 @Component({
   selector: 'app-feedbacks',
   templateUrl: './feedbacks.component.html',
-  styleUrl: './feedbacks.component.css'
+  styleUrls: ['./feedbacks.component.css']
 })
 export class FeedbacksComponent {
-
+  feedbacks: Feedback[] = [
+    {
+      name: 'Ana',
+      photo: './assets/robot.png',
+      text: 'Excelente serviço! Adorei a viagem.'
+    },
+    {
+      name: 'Pedro',
+      photo: './assets/robot.png',
+      text: 'Ótima experiência. Recomendo a todos!'
+    },
+    {
+      name: 'Maria',
+      photo: './assets/robot.png',
+      text: 'Transporte confortável e seguro. Voltarei mais vezes.'
+    },
+    {
+      name: 'Maria',
+      photo: './assets/robot.png',
+      text: 'Transporte confortável e seguro. Voltarei mais vezes.'
+    },
+    {
+      name: 'Maria',
+      photo: './assets/robot.png',
+      text: 'Transporte confortável e seguro. Voltarei mais vezes.'
+    },
+    {
+      name: 'Maria',
+      photo: './assets/robot.png',
+      text: 'Transporte confortável e seguro. Voltarei mais vezes.'
+    }
+    
+  ];
 }
