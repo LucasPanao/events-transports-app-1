6276d3970da69ce3feaaa790ead5857997771fcc
Lucas Panao <panaotrabalho@gmail.com>
2025-03-29 15:53:32 -0300

    update feedbacks component

--- src/app/menu/feedbacks/feedbacks.component.ts
+++ src/app/menu/feedbacks/feedbacks.component.ts
@@ -1,48 +1,61 @@
 import { Component } from '@angular/core';
 
-interface Feedback {
-  name: string;
-  photo: string;
-  text: string;
-}
-
 @Component({
   selector: 'app-feedbacks',
   templateUrl: './feedbacks.component.html',
   styleUrls: ['./feedbacks.component.css']
 })
 export class FeedbacksComponent {
-  feedbacks: Feedback[] = [
-    {
-      name: 'Ana',
-      photo: './assets/robot.png',
-      text: 'Excelente serviço! Adorei a viagem.'
+  customOptions: any = {
+    loop: true,
+    margin: 10,
+    autoplay: true,
+    responsiveClass: true,
+    navText: ['Anterior', 'Próximo'],
+    responsive: {
+      0: {
+        items: 1
+      },
+      480: {
+        items: 2
+      },
+      768: {
+        items: 3
+      },
+      1024: {
+        items: 4
+      },
+      1440: {
+        items: 5
+      }
     },
-    {
-      name: 'Pedro',
-      photo: './assets/robot.png',
-      text: 'Ótima experiência. Recomendo a todos!'
-    },
-    {
-      name: 'Maria',
-      photo: './assets/robot.png',
-      text: 'Transporte confortável e seguro. Voltarei mais vezes.'
-    },
-    {
-      name: 'Maria',
-      photo: './assets/robot.png',
-      text: 'Transporte confortável e seguro. Voltarei mais vezes.'
-    },
-    {
-      name: 'Maria',
-      photo: './assets/robot.png',
-      text: 'Transporte confortável e seguro. Voltarei mais vezes.'
-    },
-    {
-      name: 'Maria',
-      photo: './assets/robot.png',
-      text: 'Transporte confortável e seguro. Voltarei mais vezes.'
-    }
-    
+    nav: true,
+    dots: true
+  }
+  
+  title = 'FEEDBACKS';
+  images = [
+    {path: './assets/feedbacks/1.jpg'},
+    {path: './assets/feedbacks/2.jpg'},
+    {path: './assets/feedbacks/3.jpg'},
+    {path: './assets/feedbacks/4.jpg'},
+    {path: './assets/feedbacks/5.jpg'},
+    {path: './assets/feedbacks/6.jpg'},
+    {path: './assets/feedbacks/7.jpg'},
+    {path: './assets/feedbacks/8.jpg'},
+    {path: './assets/feedbacks/9.jpg'},
+    {path: './assets/feedbacks/10.jpg'},
+    {path: './assets/feedbacks/11.jpg'},
+    {path: './assets/feedbacks/12.jpg'},
+    {path: './assets/feedbacks/13.jpg'},
+    {path: './assets/feedbacks/14.jpg'},
+    {path: './assets/feedbacks/15.jpg'},
+    {path: './assets/feedbacks/16.jpg'},
+    {path: './assets/feedbacks/17.jpg'},
+    {path: './assets/feedbacks/18.jpg'},
+    {path: './assets/feedbacks/19.jpg'},
+    {path: './assets/feedbacks/20.jpg'},
+    {path: './assets/feedbacks/21.jpg'},
+    {path: './assets/feedbacks/22.jpg'},
   ];
 }
